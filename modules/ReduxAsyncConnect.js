a48c5aca065cd918f87a9ef5576b9ca7147cd5a3
Maxim Denisov <denisovmax1988@gmail.com>
2017-08-18 23:02:27 +0300

    Fix code-style

--- modules/ReduxAsyncConnect.js
+++ modules/ReduxAsyncConnect.js
@@ -15,147 +15,148 @@ const { array, func, object, any } = PropTypes;
  * @param iterator
  */
 function eachComponents(components, iterator) {
-  for (let i = 0, l = components.length; i < l; i++) { // eslint-disable-line id-length
-    if (typeof components[i] === 'object') {
-      for (let [key, value] of Object.entries(components[i])) {
-        iterator(value, i, key);
-      }
-    } else {
-      iterator(components[i], i);
-    }
-  }
+	for (let i = 0, l = components.length; i < l; i++) { // eslint-disable-line id-length
+		if (typeof components[i] === 'object') {
+			for (let [key, value] of Object.entries(components[i])) {
+				iterator(value, i, key);
+			}
+		} else {
+			iterator(components[i], i);
+		}
+	}
 }
 
 function filterAndFlattenComponents(components) {
-  const flattened = [];
-  eachComponents(components, (Component) => {
-    if (Component && Component.reduxAsyncConnect) {
-      flattened.push(Component);
-    }
-  });
-  return flattened;
+	const flattened = [];
+	eachComponents(components, (Component) => {
+		if (Component && Component.reduxAsyncConnect) {
+			flattened.push(Component);
+		}
+	});
+	return flattened;
 }
 
-function loadAsyncConnect({components, filter = () => true, ...rest}) {
-  let async = false;
+function loadAsyncConnect({ components, filter = () => true, ...rest }) {
+	let async = false;
 
-  if (!filterAndFlattenComponents(components).some(c => c.reduxAsyncConnect.length > 0)) {
-      async = true;
-      return { promise: Promise.resolve([]), async: async };
-  }
+	if (!filterAndFlattenComponents(components).some(c => c.reduxAsyncConnect.length > 0)) {
+		async = true;
+		return { promise: Promise.resolve([]), async: async };
+	}
 
-  const promise = Promise.all(filterAndFlattenComponents(components).map(Component => {
-    const asyncItems = Component.reduxAsyncConnect;
+	const promise = Promise.all(filterAndFlattenComponents(components).map(Component => {
+		const asyncItems = Component.reduxAsyncConnect;
 
-    return Promise.all(asyncItems.reduce((itemsResults, item) => {
-      let promiseOrResult = item.promise(rest);
+		return Promise.all(asyncItems.reduce((itemsResults, item) => {
+			let promiseOrResult = item.promise(rest);
 
-      if (filter(item, Component)) {
-        if (promiseOrResult && promiseOrResult.then instanceof Function) {
-          async = true;
-          promiseOrResult = promiseOrResult.catch(error => ({error}));
-        }
-        return [...itemsResults, promiseOrResult];
-      } else {
-        return itemsResults;
-      }
-    }, [])).then(results => {
-      return asyncItems.reduce((result, item, i) => ({...result, [item.key]: results[i]}), {});
-    });
-  }));
+			if (filter(item, Component)) {
+				if (promiseOrResult && promiseOrResult.then instanceof Function) {
+					async = true;
+					promiseOrResult = promiseOrResult.catch(error => ({ error }));
+				}
+				return [...itemsResults, promiseOrResult];
+			} else {
+				return itemsResults;
+			}
+		}, [])).then(results => {
+			return asyncItems.reduce((result, item, i) => ({ ...result, [item.key]: results[i] }), {});
+		});
+	}));
 
-  return {promise, async};
+	return { promise, async };
 }
 
 export function loadOnServer(args) {
-  const result = loadAsyncConnect(args);
-  if (result.async) {
-    result.promise.then(() => {
-      args.store.dispatch(endGlobalLoad());
-    });
-  }
-  return result.promise;
+	const result = loadAsyncConnect(args);
+	if (result.async) {
+		result.promise.then(() => {
+			args.store.dispatch(endGlobalLoad());
+		});
+	}
+	return result.promise;
 }
 
 let loadDataCounter = 0;
 
 class ReduxAsyncConnect extends Component {
-  static propTypes = {
-    components: array.isRequired,
-    params: object.isRequired,
-    render: func.isRequired,
-    beginGlobalLoad: func.isRequired,
-    endGlobalLoad: func.isRequired,
-    helpers: any
-  };
+	static propTypes = {
+		components: array.isRequired,
+		params: object.isRequired,
+		render: func.isRequired,
+		beginGlobalLoad: func.isRequired,
+		endGlobalLoad: func.isRequired,
+		helpers: any
+	};
 
-  static contextTypes = {
-    store: object.isRequired
-  };
+	static contextTypes = {
+		store: object.isRequired
+	};
 
-  static defaultProps = {
-    render(props) {
-      return <RouterContext {...props} />;
-    }
-  };
+	static defaultProps = {
+		render(props) {
+			return <RouterContext { ...props } />;
+		}
+	};
 
-  isLoaded() {
-    return this.context.store.getState().reduxAsyncConnect.loaded;
-  }
+	isLoaded() {
+		return this.context.store.getState().reduxAsyncConnect.loaded;
+	}
 
-  constructor(props, context) {
-    super(props, context);
+	constructor(props, context) {
+		super(props, context);
 
-    this.state = {
-      propsToShow: this.isLoaded() ? props : null
-    };
-  }
+		this.state = {
+			propsToShow: this.isLoaded() ? props : null
+		};
+	}
 
-  componentDidMount() {
-    const dataLoaded = this.isLoaded();
+	componentDidMount() {
+		const dataLoaded = this.isLoaded();
 
-    if (!dataLoaded) { // we dont need it if we already made it on server-side
-      this.loadAsyncData(this.props);
-    }
-  }
+		if (!dataLoaded) { // we dont need it if we already made it on server-side
+			this.loadAsyncData(this.props);
+		}
+	}
 
-  componentWillReceiveProps(nextProps) {
-    this.loadAsyncData(nextProps);
-  }
+	componentWillReceiveProps(nextProps) {
+		this.loadAsyncData(nextProps);
+	}
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.state.propsToShow !== nextState.propsToShow;
-  }
+	shouldComponentUpdate(nextProps, nextState) {
+		return this.state.propsToShow !== nextState.propsToShow;
+	}
 
-  loadAsyncData(props) {
-    const store = this.context.store;
-    const loadResult = loadAsyncConnect({...props, store});
+	loadAsyncData(props) {
+		const store = this.context.store;
+		const loadResult = loadAsyncConnect({ ...props, store });
 
-    loadDataCounter++;
+		loadDataCounter++;
 
-    if (loadResult.async) {
-      this.props.beginGlobalLoad();
-      (loadDataCounterOriginal => {
-        loadResult.promise.then(() => {
-          // We need to change propsToShow only if loadAsyncData that called this promise
-          // is the last invocation of loadAsyncData method. Otherwise we can face situation
-          // when user is changing route several times and we finally show him route that has
-          // loaded props last time and not the last called route
-          if (loadDataCounter === loadDataCounterOriginal) {
-            this.setState({propsToShow: props});
-          }
-          this.props.endGlobalLoad();
-        });
-      })(loadDataCounter);
-    } else {
-      this.setState({propsToShow: props});
-    }
-  }
+		if (loadResult.async) {
+			this.props.beginGlobalLoad();
+			(loadDataCounterOriginal => {
+				loadResult.promise.then(() => {
+					// We need to change propsToShow only if loadAsyncData that called this promise
+					// is the last invocation of loadAsyncData method. Otherwise we can face situation
+					// when user is changing route several times and we finally show him route that has
+					// loaded props last time and not the last called route
+					if (loadDataCounter === loadDataCounterOriginal) {
+						this.setState({ propsToShow: props });
+					}
 
-  render() {
-    const {propsToShow} = this.state;
-    return propsToShow && this.props.render(propsToShow);
-  }
+					this.props.endGlobalLoad();
+				});
+			})(loadDataCounter);
+		} else {
+			this.setState({ propsToShow: props });
+		}
+	}
+
+	render() {
+		const { propsToShow } = this.state;
+		return propsToShow && this.props.render(propsToShow);
+	}
 }
 
-export default connect(null, {beginGlobalLoad, endGlobalLoad})(ReduxAsyncConnect);
+export default connect(null, { beginGlobalLoad, endGlobalLoad })(ReduxAsyncConnect);
--- modules/index.js
+++ modules/index.js
@@ -1,11 +1,11 @@
-import ReduxAsyncConnect, {loadOnServer} from './ReduxAsyncConnect';
-import {reducer, clearKey, loadSuccess, asyncConnect} from './asyncConnect';
+import ReduxAsyncConnect, { loadOnServer } from './ReduxAsyncConnect';
+import { reducer, clearKey, loadSuccess, asyncConnect } from './asyncConnect';
 
 export {
-    loadOnServer,
-    ReduxAsyncConnect,
-    reducer,
-    clearKey,
-    loadSuccess,
-    asyncConnect
+  loadOnServer,
+  ReduxAsyncConnect,
+  reducer,
+  clearKey,
+  loadSuccess,
+  asyncConnect
 };
